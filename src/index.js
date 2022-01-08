5e9d181f743bf708719da8004a75dd0e909fead3
Peter-32 <peter@impactradius.com>
2022-01-07 21:03:11 -0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+const avatar = require('./images/blank_profile.png');
+
+const Comment = function(props) {
+    return (
+      <div className="ui container comments">
+          <div className="comment">
+              <a href="/" className="avatar">
+                  <img alt="avatar" src={faker.image.image()}/>
+              </a>
+              <div className="content">
+                  <a href="/" className="author">{props.name}</a>
+                  <div className="metadata">
+                      <span className="date">{props.time}</span>
+                  </div>
+                  <div className="text">{props.comment_detail}</div>
+              </div>
+          </div>
+      </div>
+  );
+}
+
+const App = function() {
+  return (
+      <div>
+          <Comment name="Cheryl" time="2021-01-03 00:00:00" comment_detail="hello" className="ui container comment"></Comment>
+          <Comment name="Peter" time="2021-01-02 00:00:00" comment_detail="hi" className="ui container comment"></Comment>
+          <Comment name="Jithin" time="2021-01-01 00:00:00" comment_detail=":)" className="ui container comment"></Comment>
+      </div>
+  );
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
